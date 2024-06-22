a45a427513fddf72893ec6da8e23dec9269c049e
Mitsunori Komatsu <komamitsu@gmail.com>
2024-06-22 16:12:38 +0900

    Improve value conversion

--- src/main/java/org/komamitsu/fastuuidparser/FastUuidParser.java
+++ src/main/java/org/komamitsu/fastuuidparser/FastUuidParser.java
@@ -49,32 +49,20 @@ public final class FastUuidParser
 
     private static byte hexToDigit(String s, int position)
     {
-        switch (s.charAt(position)) {
-            case '-': return -1;
-            case '0': return 0;
-            case '1': return 1;
-            case '2': return 2;
-            case '3': return 3;
-            case '4': return 4;
-            case '5': return 5;
-            case '6': return 6;
-            case '7': return 7;
-            case '8': return 8;
-            case '9': return 9;
-            case 'a':
-            case 'A': return 10;
-            case 'b':
-            case 'B': return 11;
-            case 'c':
-            case 'C': return 12;
-            case 'd':
-            case 'D': return 13;
-            case 'e':
-            case 'E': return 14;
-            case 'f':
-            case 'F': return 15;
-            default:  throw new IllegalArgumentException(String.format("Invalid UUID-format at position %d: %s", position, s));
+        char c = s.charAt(position);
+        if (c == '-') {
+            return -1;
         }
+        if (c >= '0' && c <= '9') {
+            return (byte) (c - '0');
+        }
+        if (c >= 'a' && c <= 'f') {
+            return (byte) (c - 'a' + 10);
+        }
+        if (c >= 'A' && c <= 'F') {
+            return (byte) (c - 'A' + 10);
+        }
+        throw new IllegalArgumentException(String.format("Invalid UUID-format at position %d: %s", position, s));
     }
 }
 
